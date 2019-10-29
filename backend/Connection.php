fc2ae247d019fcd72c0c819c800d31099323ef6d
Grant K <grantkveton@msn.com>
2019-10-29 16:48:59 -0400

    connection.php edits

--- backend/Connection.php
+++ backend/Connection.php
@@ -1,1 +1,14 @@
 <?php
+/* Connect to a MySQL database using driver invocation */
+$dsn = 'mysql:dbname=cen4010fal19_g06;host=lamp.cse.fau.edu';
+$user = 'cen4010fal19_g06';
+$password = 'nKg19Fa06';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    echo($dbh);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+?>
