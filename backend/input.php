0e15c8c3c1898a1eae4eb39c83b30219d3f7af38
Kevin Fash <54957416+kfash1@users.noreply.github.com>
2019-11-03 17:30:35 -0800

    class
    added a class to the input.php

--- backend/input.php
+++ backend/input.php
@@ -7,13 +7,57 @@
 <body>
 
 <?php
-    $post = $_POST['post'];
+    class DBController
+    {
+        //property declaration
+        public $type = "";
+        public $title ="";
+        public $description ="";
+
+        //methods
+        //getters and setters
+        public function setType($type)
+        {
+            $this->type = $type;
+        }
+        public function getType()
+        {
+            return $this->type;
+        }
+        public function setTitle($title)
+        {
+            $this->title = $title;
+        }
+        public function getTitle()
+        {
+            return $this->title;
+        }
+        public function setDescription($description)
+        {
+            $this->description = $description;
+        }
+        public function getDescription()
+        {
+            return $this->description;
+        }
+
+
+    }
+
+    $post = new DBController();
+    $post->setType($_POST['type']);
+    $post->setTitle($_POST['title']);
+    $post->setDescription($_POST['description']);
+
+    $type = $_POST['type'];
     $title = $_POST['title'];
     $description = $_POST['description'];
     
-    echo $post . '<br><br>';
-    echo $title . '<br><br>';
-    echo $description . '.' . '<br><br>';
+    echo $post->getType() . '<br><br>';
+    echo $post->getTitle() . '<br><br>';
+    echo $post->getDescription() . '<br><br>';
+    //echo $title . '<br><br>';
+    //echo $description . '.' . '<br><br>';
     
 ?>
 
