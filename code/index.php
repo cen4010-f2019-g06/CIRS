7a18bcc27520ea28bd31852d3557637ff78b0037
Grant K <grantkveton@msn.com>
2019-11-17 21:55:23 -0500

    updated index.php to reflect current state on server

--- code/index.php
+++ code/index.php
@@ -1,7 +1,8 @@
-<!--
-<?php include ("assests/pages/nav.php")?>
-<?php $title = "Dashboard"; ?> 
--->
+<?php
+include 'DBController.php';
+include 'Credentials.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,9 +14,10 @@
 </head>
 
 <body>
+    <?php include 'assets/pages/nav.php'?>
     <!-- Header Bar-->
     <header class="header-bar">
-        <a href="index.html" class="logo">[cirs logo]</a>
+        <a href="index.php" class="logo">[cirs logo]</a>
 
         <img src="assets/images/buttons/menu-collapsed.svg" class="menu-bttn" id="menu-closed" onclick="openNav()">
 
@@ -25,10 +27,6 @@
         </div>
     </header>
     <div class="horizontal-line"></div>
-    
-    
-    
-    
 
     <!-- Filter/Create Post-->
     <div class="dash-header">
@@ -51,11 +49,7 @@
             </div>
         </div>
     </div>
-    
-    
-    
-    
-    
+
     <div class="center-element">
         <div class="dash-post-container">
             <!-- Left Page Nav -->
@@ -64,225 +58,59 @@
                     <img class="arrow-size" src="assets/images/buttons/next-page-left.svg">
                 </button>
             </div>
-            
+
             <!-- Post Grid -->
             <div class="grid">
-        <!-- #1 -->
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
+                <?php
+                /*
+                 * Dynamically outputs 12 posts by filling HTML template with DB data
+                 */
 
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
+                $db = new DBController($DB_USER, $DB_PASSWORD); //Establish connection to DB with controller
 
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
+                $issuePostsArray = $db->getIssuePosts(); //get array of Issue posts from DB using controller
 
-        <!-- #2 -->
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-in-progress.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
+                //for each Post in the DB, display it on the page
+                for($i=0; $i<12; $i++)
+                {
+                    if ($issuePostsArray[$i]->getStatus() == "Reviewed")
+                    {
+                        $statusImage = "assets/images/icons/status/status-reviewed.svg";
+                    }
+                    else if($issuePostsArray[$i]->getStatus() == "In Progress")
+                    {
+                        $statusImage = "assets/images/icons/status/status-in-progress.svg";
+                    }
+                    else if($issuePostsArray[$i]->getStatus() == "Pending")
+                    {
+                        $statusImage = "assets/images/icons/status/status-pending.svg";
+                    }
+                    else if($issuePostsArray[$i]->getStatus() == "Done")
+                    {
+                        $statusImage = "assets/images/icons/status/status-done.svg";
+                    }
+                    else if($issuePostsArray[$i]->getStatus() == "None")
+                    {
+                        $statusImage = "assets/images/icons/status/status-none.svg";
+                    }
 
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #3 -->  
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-reviewed.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Sep 22</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/not-watching.svg"> </div>
-                    </footer>
-                </article>
-        <!-- #4 -->     
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #5 -->
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #6 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #7 -->      
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #8 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #9 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #10 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #11 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
-        <!-- #12 -->   
-                <article class="post-tile column-1-fourth">
-                    <div class="post-tile__header">
-                        <img class="post-tile__status" src="assets/images/icons/status/status-pending.svg">
-                        <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
-                    </div>
-
-                    <div class="post-tile__body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <!-- php post content var (db) (20 characters max)-->
-                    </div>
-
-                    <footer>
-                        <div class="post-tile__date"><time datetime="2019-08-30">Aug 30</time> <!-- php date var (db) -->
-                        </div>
-                        <div class="post-tile__watching"><img src="assets/images/icons/indicators/watching.svg">4</div>
-                    </footer>
-                </article>
+                    echo '<article class="post-tile column-1-fourth">'.
+                        '            <div class="post-tile__header">
+                <img class="post-tile__status" src="' . $statusImage . '">
+                <img src="assets/images/icons/default-user-icon.svg" class="user-avatar">
+                </div>' .
+                        '<div class="post-tile__body">
+                <p>' . substr($issuePostsArray[$i]->getContent(), 1, 20) . '...</p>
+                <!-- php post content var (db) (20 characters max)-->
+                </div>' .
+                        '            <footer>
+                <div class="post-tile__date"><time datetime="2019-08-30">'. $issuePostsArray[$i]->getTime(). '</time> <!-- php date var (db) -->
+                </div>
+                <div class="post-tile__watching"><img src='. $issuePostsArray[$i]->getWatchIcon() .'>'. $issuePostsArray[$i]->getWatchCount().'</div>
+                </footer>
+                </article>';
+                }
+                ?>
             </div>
 
             <!-- Right Page Nav -->
@@ -293,38 +121,36 @@
             </div>
 
             <!-- Advice Column -->
-            <div class="advice-col-container">
-                <div class="advice-col-box">
-                    <img class="advice-icon" src="assets/images/icons/indicators/advice-indicator.svg">
-                <div class="advice-col-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua amore botem eda?</div>
-                <footer>
-                    <div class="advice-col__date"><time datetime="2019-08-30">Aug 30</time>
-                        </div>
-                        <div class="advice-col__comments">4 comments<img class="comment-icon" src="assets/images/icons/comment-icon.svg"></div>
-                    </footer>
-                </div>
-                
-                
-                
-                <div class="advice-col-box">
-                    <img class="advice-icon" src="assets/images/icons/indicators/advice-indicator.svg"></div>
-                <div class="advice-col-box"></div>
-                <div class="advice-col-box"></div>
-                <div class="advice-col-box"></div>
-            </div>
-            
+            <?php
+                include 'input.php';
+
+                $db = new DBController($DB_USER, $DB_PASSWORD); //Establish connection to DB with controller
+
+                $postsArray = $db->getPosts(); //get array of posts from DB using controller
+
+                //for each Post in the DB, display it on the page
+                foreach($postsArray as $p)
+                {
+                    echo    "<div class='advice-col-container'>
+                                <div class='advice-col-box'>
+                                    <img class='advice-icon' src='assets/images/icons/indicators/advice-indicator.svg'>
+                                    <div class='advice-col-body'>" . $p->getContent() . "</div>
+                                    <footer>
+                                        <div class='advice-col__date'><time datetime='" . $p->getDate() . "'>" . $p->getDate() . "</time>
+                                        </div>
+                                        <div class='advice-col__comments'>" . $p->getCommentCount . "comments<img class='comment-icon' src='assets/images/icons/comment-icon.svg'></div>
+                                    </footer>
+                                </div>
+                            </div>";
+            }?>
             <div class="clear-floats"></div>
             <input class="page-num-container" placeholder="1" disabled>
         </div>
-    </div> 
-
-    
-    
-    
-    
+    </div>
 
     <!-- include google's jquery hosted library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="assets/scripts/main.js"></script>
 </body>
+
 </html>
