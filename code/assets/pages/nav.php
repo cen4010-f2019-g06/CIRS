1fec6f94fda7bfc53f1c3f6399e1abc4bda70125
kfash1 <kfash1@gmail.com>
2019-11-17 16:31:47 -0800

    Update nav.php

--- code/assets/pages/nav.php
+++ code/assets/pages/nav.php
@@ -5,24 +5,36 @@
     </header>
     <div class="horizontal-line"></div>
 
+    <!-- Meet the creators -->
     <div class="creator-box">
         <h3 class="creators-info">Meet The Creators</h3>
         <div class="hl-spacing"></div>
         <div class="horizontal-line"></div>
         <ul class="creator-container">
+            <!-- Grant -->
             <li><img src="/~cen4010fal19_g06/assets/images/creators/grant-icon.png" class="creators-icon">
                 <div class="creators-intro">
-                    <p>My name is Grant and I am a senior level Computer Science student at Florida Atlantic University. During the summer of this year, I had the opportunity to intern with Amazon Web Services, where I was exposed to real life development situations and cloud technology...</p>
+                    <p>My name is Grant and I am a senior level Computer Science student at Florida Atlantic University. 
+                        During the summer of this year, I had the opportunity to intern with Amazon Web Services, 
+                        where I was exposed to real life development situations and cloud technology...</p>
                 </div>
-            </li> <!-- Grant -->
+            </li> 
+            <!-- Kevin -->
             <li><img src="/~cen4010fal19_g06/assets/images/creators/kevin-icon.png" class="creators-icon">
                 <div class="creators-intro">
-                    <p>My name is Kevin and most of my time is split up between working full-time as a Manager of Train Operations (MTO) and going to school. One of the reasons I chose Computer Science is because I've always enjoyed working around computers, but it is also one of the few majors I could do around my hectic work schedule...</p>
+                    <p>My name is Kevin and most of my time is split up between working full-time as a 
+                        Manager of Train Operations (MTO) and going to school. One of the reasons I chose 
+                        Computer Science is because I've always enjoyed working around computers, but it 
+                        is also one of the few majors I could do around my hectic work schedule...</p>
                 </div>
             </li>
+            <!-- Nicole -->
             <li><img src="/~cen4010fal19_g06/assets/images/creators/nicole-icon.png" class="creators-icon">
                 <div class="creators-intro">
-                    <p>My name is Nicole and I'm currently a CE major at FAU, going into her senior year. Prior to becoming a CE major, I wanted to attend art school. Now I'm interested in working closely with embedded systems to help develop the artificial limbs of the future.</p>
+                    <p>My name is Nicole and I'm currently a CE major at FAU, going into her senior year. 
+                        Prior to becoming a CE major, I wanted to attend art school. Now I'm interested 
+                        in working closely with embedded systems to help develop the artificial limbs of 
+                        the future.</p>
                 </div>
             </li>
         </ul>
