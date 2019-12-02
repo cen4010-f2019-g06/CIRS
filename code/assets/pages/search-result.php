5e2ed1618c38570e8525412070f2169282648f06
Kevin Fash <54957416+kfash1@users.noreply.github.com>
2019-12-02 16:47:56 -0500

    Update search-result.php

--- code/assets/pages/search-result.php
+++ code/assets/pages/search-result.php
@@ -1,3 +1,8 @@
+<?php
+include '/home/cen4010fal19_g06/public_html/checkLogin.php';
+include_once '/home/cen4010fal19_g06/public_html/DBConnection.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,14 +10,14 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="../stylesheets/main.css">
     <link rel="stylesheet" href="https://use.typekit.net/xkf2xga.css">
-    <title>Dashboard</title>
+    <title>Search Results</title>
 </head>
 
 <body>
     <?php include 'nav.php'?>
     <!-- Header Bar-->
     <header class="header-bar">
-        <a href="../../index.html" class="logo">[cirs logo]</a>
+        <a href="/~cen4010fal19_g06/index.html" class="logo">[cirs logo]</a>
 
         <img src="../images/buttons/menu-collapsed.svg" class="menu-bttn" id="menu-closed" onclick="openNav()">
 
@@ -23,68 +28,53 @@
     </header>
     <div class="horizontal-line"></div>
 
+    <?php
+    if(isset($_POST['query']))
+    {
+        $query = strtolower($_POST['query']);
+        $results = $db->searchIssuePosts($query);
 
-    <div class="center-element">
+        echo '
+        <div class="center-element">
         <div class="search-post-container">
             <div class="search-result-header">
-                <h3>Search results for "<!-- search goes here-->"</h3>
+                <h3>Search results for "' . $query .'"</h3>
             </div>
-            
             <div class="search-grid center-element">
-                <!-- Search Item #1 -->
-                <div class="search-item-header">
-                    <img class="search-user-icon" src="../images/icons/default-user-icon.svg">
-                    <p class="search-user"><span style="color: var(--faded-navy)">winkblink</span></p>
-                </div>
-                
-                <div class="search-item-body">
-                    <h3 class="search-title">Transport App is Garbage</h3>
-                    <p><!-- this section will be nil for advice posts-->I often get asked about why I use Vim as my primary editor, there is no particular reason for this, except that I ended up learning it when I moved over to Linux full time many years ago. I ended up liking it because I could edit my small source files on my quad-core machine without needing to wait forever for the file to open.
-                        Sure Vim isn’t a bad editor, it’s highly extensible, it’s easy...</p>
-                </div>
-                
-                <div class="search-item-footer">
-                    <time class="search-item-date">Sep 2019</time>
-                    <div class="search-item-watch" ><img src="../images/icons/indicators/watching.svg">20</div>
-                </div>
-                <div class="horizontal-line"></div>
+        ';
 
-                <!-- Search Item #2 -->
+        if(count($results) == 0)
+        {
+            echo "No search results";
+        }
+        else
+        {
+            echo count($results) . " search results <br>";
+            foreach($results as $result)
+            {
+                $postUser = $db->getUserByZNumber($result->getPostedByZNum());
+                echo '
+                <!-- Search Item -->
                 <div class="search-item-header">
                     <img class="search-user-icon" src="../images/icons/default-user-icon.svg">
-                    <p class="search-user"><span style="color: var(--faded-navy)">winkblink</span></p>
+                    <p class="search-user"><span style="color: var(--faded-navy)">'. $postUser['username'] . '</span></p>
                 </div>
                 
                 <div class="search-item-body">
-                    <h3 class="search-title">Transport App is Garbage</h3>
-                    <p>I often get asked about why I use Vim as my primary editor, there is no particular reason for this, except that I ended up learning it when I moved over to Linux full time many years ago. I ended up liking it because I could edit my small source files on my quad-core machine without needing to wait forever for the file to open.
-                        Sure Vim isn’t a bad editor, it’s highly extensible, it’s easy...</p>
+                    <h3 class="search-title">' . $result->getTitle() . '</h3>
+                    <p>' . $result->getContent() .'</p>
                 </div>
                 
                 <div class="search-item-footer">
-                    <time class="search-item-date">Sep 2019</time>
-                    <div class="search-item-watch" ><img src="../images/icons/indicators/watching.svg">20</div>
+                    <time class="search-item-date">'. $result->getTime() . '</time>
+                    <div class="search-item-watch" ><img src='. $db->queryWatchIcon($result->getWatchId()) . '>'. $result->getWatchCount() . '</div>
                 </div>
                 <div class="horizontal-line"></div>
-                
-                <!-- Search Item #3 -->
-                <div class="search-item-header">
-                    <img class="search-user-icon" src="../images/icons/default-user-icon.svg">
-                    <p class="search-user"><span style="color: var(--faded-navy)">winkblink</span></p>
-                </div>
-                
-                <div class="search-item-body">
-                    <h3 class="search-title">Transport App is Garbage</h3>
-                    <p>I often get asked about why I use Vim as my primary editor, there is no particular reason for this, except that I ended up learning it when I moved over to Linux full time many years ago. I ended up liking it because I could edit my small source files on my quad-core machine without needing to wait forever for the file to open.
-                        Sure Vim isn’t a bad editor, it’s highly extensible, it’s easy...</p>
-                </div>
-                
-                <div class="search-item-footer">
-                    <time class="search-item-date">Sep 2019</time>
-                    <div class="search-item-watch" ><img src="../images/icons/indicators/watching.svg">20</div>
-                </div>
-                <div class="horizontal-line"></div>
-                
+                ';
+            }
+        }
+    }
+    ?>
                     <div class="center-element">
                         <div class="search-nav-tool">
                             <!-- Left Page Nav -->
@@ -100,13 +90,13 @@
             </div>
 
 
-            
-            
+
+
         </div>
 
     <!-- include google's jquery hosted library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="assets/scripts/main.js"></script>
+    <script src="../scripts/main.js"></script>
 </body>
 
 </html>
