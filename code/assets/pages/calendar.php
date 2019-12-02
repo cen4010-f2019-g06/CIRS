62ed8e86548b78ca94ae4edc9d8c80808f51e980
Kevin Fash <54957416+kfash1@users.noreply.github.com>
2019-12-02 13:29:13 -0500

    Update calendar.php

--- code/assets/pages/calendar.php
+++ code/assets/pages/calendar.php
@@ -1,4 +1,7 @@
-<?php include ("nav.php")?>
+<?php 
+include ("nav.php");
+include_once '/home/cen4010fal19_g06/public_html/DBConnection.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -42,95 +45,112 @@
     </header>
     <div class="horizontal-line"></div>
     
-
-    
-    <!--Calendar Stuff-->
     <?php
-    // Set your timezone
-    date_default_timezone_set('America/New_York');
-    // Get prev & next month
-    if (isset($_GET['ym'])) {
-        $ym = $_GET['ym'];
-    } else {
-        // This month
-        $ym = date('Y-m');
-    }
-    // Check format
-    $timestamp = strtotime($ym . '-01');
-    if ($timestamp === false) {
-        $ym = date('Y-m');
-        $timestamp = strtotime($ym . '-01');
-    }
-    // Today
-    $today = date('Y-m-j', time());
-    // For H3 title
-    $html_title = date('Y / m', $timestamp);
-    // Create prev & next month link
-    $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
-    $next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
-
-    // Number of days in the month
-    $day_count = date('t', $timestamp);
-    
-    // 0:Sun 1:Mon 2:Tue ...
-    $str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
-
-    // Create Calendar
-    $weeks = array();
-    $week = '';
-    
-    //Creat array of events
-    $events = array();
-    
-
-    // Add empty cell
-    $week .= str_repeat('<td></td>', $str);
-    for ( $day = 1; $day <= $day_count; $day++, $str++) {
-        
-        $date = $ym . '-' . $day;
-        
-        if ($today == $date) {
-            $week .= '<td class="today">' . $day . '<br /><br />' . 'testing';
+        // Set your timezone
+        date_default_timezone_set('America/New_York');
+        // Get prev & next month
+        if (isset($_GET['ym'])) {
+            $ym = $_GET['ym'];
         } else {
-            $week .= '<td>' . $day . '<br /><br />' . 'testing'; //this is where the events get inserted
+            // This month
+            $ym = date('Y-m');
         }
-        $week .= '</td>';
-        
-        // End of the week OR End of the month
-        if ($str % 7 == 6 || $day == $day_count) {
-            if ($day == $day_count) {
-                // Add empty cell
-                $week .= str_repeat('<td></td>', 6 - ($str % 7));
-            }
-            $weeks[] = '<tr>' . $week . '</tr>';
-            // Prepare for new week
-            $week = '';
+        // Check format
+        $timestamp = strtotime($ym . '-01');
+        if ($timestamp === false) {
+            $ym = date('Y-m');
+            $timestamp = strtotime($ym . '-01');
         }
-    }
-    ?>
-    <div class="calendar center-element">
-    <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
-        <table class="calendar-container">
-            <tr>
-                <th>S</th>
-                <th>M</th>
-                <th>T</th>
-                <th>W</th>
-                <th>T</th>
-                <th>F</th>
-                <th>S</th>
-            </tr>
-            <?php
-                foreach ($weeks as $week) {
-                    echo $week;
-                }
-            ?>
-        </table>
-    </div>
-    
-    <br>
-    <br>
+        // Today
+        $today = date('Y-m-j', time());
+        // For H3 title
+        $html_title = date('Y / m', $timestamp);
+        // Create prev & next month link
+        $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+        $next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
 
+        // Number of days in the month
+        $day_count = date('t', $timestamp);
+
+        // 0:Sun 1:Mon 2:Tue ...
+        $str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+
+        // Create Calendar
+        $weeks = array();
+        $week = '';
+        
+        
+        //Create array of events
+        $eventPostsArray = $db->getEventPosts();
+        function findEventsByDate($date)
+        {
+            foreach ( $eventPostsArray as $element ) {
+                if ($date == $element->eventDate) {
+                    return $element;
+                }
+            }
+            return false;
+        }
+        
+        // Add empty cell
+        $week .= str_repeat('<td class="calendar-cell-row"></td>', $str);
+        for ( $day = 1; $day <= $day_count; $day++, $str++) {
+
+            $date = $ym . '-' . $day;
+
+            if ($today == $date) {
+                $week .= '<td class="today">' . $day;
+            } else {
+                $week .= '<td class="calendar-cell-row">' . $day;
+            }
+            $week .= '</td>';
+
+            // End of the week OR End of the month
+            if ($str % 7 == 6 || $day == $day_count) {
+                if ($day == $day_count) {
+                    // Add empty cell
+                    $week .= str_repeat('<td class="calendar-cell-row"></td>', 6 - ($str % 7));
+                }
+                $weeks[] = '<tr class="calendar-rows__large">' . $week . '</tr>';
+                // Prepare for new week
+                $week = '';
+            }
+        }
+        ?>
+    
+        <div class="calendar center-element">        
+                <div class="calendar-nav center-element">
+                   
+                	<a href="?ym=<?php echo $prev; ?>">
+                        	<button class="calendar-l-arrow">
+                            		<img class="arrow-size" src="../images/buttons/next-page-left.svg"></button></a>
+ 			<a href="?ym=<?php echo $next; ?>">
+                        	<button class="calendar-r-arrow">
+                            		<img class="arrow-size" src="../images/buttons/next-page-right.svg"></button></a>
+                </div>
+            <table class="calendar-container">
+                <caption class="calendar-month__large"><?php echo $html_title; ?></caption>
+                <thead>
+                    <tr>
+                        <th class="calendar-col__large calendar-cell" scope="col">Mon</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Tues</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Wed</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Thurs</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Fri</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Sat</th>
+                        <th class="calendar-col__large calendar-cell" scope="col">Sun</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($weeks as $week) {
+                            echo $week;
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+   
     <!-- include google's jquery hosted library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../scripts/main.js"></script>
