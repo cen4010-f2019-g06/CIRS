8d65870c039061277d0ea2fedfee3564b362cb13
Grant K <grantkveton@msn.com>
2019-12-02 18:16:03 -0500

    searchbar.php merge conflict resolved

--- code/assets/pages/searchbar.php
+++ code/assets/pages/searchbar.php
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
-<div class="search-bar">
-    <form action="assets/pages/search-result.php" method="post">
-        <img class="search-icon" src="assets/images/icons/search-icon.svg">
-        <input type="text" name="query" class="search" placeholder="Search" maxlength="200" required>
-        <input type="submit" value="Search">
-    </form>
-=======
 <div class="search-bar">
     <form action="/~cen4010fal19_g06/assets/pages/search-result.php" method="post">
         <img class="search-icon" src="/~cen4010fal19_g06/assets/images/icons/search-icon.svg">
         <input type="text" name="query" class="search" placeholder="Search" maxlength="200" required>
         <input type="submit" value="Search">
     </form>
->>>>>>> Stashed changes
 </div>
